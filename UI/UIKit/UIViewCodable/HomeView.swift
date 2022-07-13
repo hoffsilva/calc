7a34f20eff4449fe2dfcb5a965d25028febeeebf
Hoff Silva <hoff.henry@gmail.com>
2022-07-13 14:23:16 -0400

    Added button the home screen

--- UI/UIKit/UIViewCodable/HomeView.swift
+++ UI/UIKit/UIViewCodable/HomeView.swift
@@ -9,6 +9,17 @@ import UIKit
 
 final class HomeView: UIView {
     
+    private lazy var buttonSize: CGSize = {
+        CGSize(width: 78, height: 78)
+    }()
+    
+    private lazy var button: UIButton = {
+        UIButtonBuilderImp()
+            .withTitle("1", state: .normal)
+            .withBackgroundColor(.systemYellow)
+            .build()
+    }()
+    
     // MARK: - Initialization
     
     init() {
@@ -25,11 +36,11 @@ final class HomeView: UIView {
 extension HomeView: UIViewCodable {
     
     func setupViewsHierarchy() {
-        
+        addSubview(button)
     }
     
     func setupConstraints() {
-        
+        button.setSize(size: buttonSize)
     }
     
 }
