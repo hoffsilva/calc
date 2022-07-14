75c8572209b4bcf0ea4044ebc401bbbcca5ee971
Hoff Silva <hoff.henry@gmail.com>
2022-07-14 19:09:22 -0400

    Create iOS App Target

---
+++ Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calc
+//
+//  Created by Hoff Silva on 2022-07-14.
+//
+
+import UIKit
+import UI
+
+class ViewController: UIViewController {
+    
+    override func loadView() {
+        super.loadView()
+        self.view = HomeView()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
