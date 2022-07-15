b15871c2a4e7df58c800780cba1bbf24c7b9919c
Hoff Silva <hoff.henry@gmail.com>
2022-07-15 17:38:03 -0400

    Created CalculatorTheme

---
+++ UI/UIKit/Theme/CalculatorTheme.swift
@@ -0,0 +1,25 @@
+//
+//  CalculatorTheme.swift
+//  UI
+//
+//  Created by Hoff Silva on 2022-07-15.
+//
+
+import UIKit
+
+public struct CalculatorTheme {
+    
+    public init() {}
+    
+    let backgroundColor = UIColor.lightGray
+    let displayColor = UIColor.white
+    
+    let extraFunctionColor = UIColor.lightGray
+    let extraFunctionTitleColor = UIColor.white
+    
+    let operationColor = UIColor.orange
+    let operationTitleColor = UIColor.white
+    
+    let pinpadColor = UIColor.darkGray
+    let pinpadTitleColor = UIColor.white
+}
